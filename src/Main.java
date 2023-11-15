da531baf9d08c1a844a93e610ea997369552e4a2
Oleg Ten <tennet0505@gmail.com>
2023-11-15 14:24:40 +0100

    Tic Tac Toe. Players names

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,40 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Main.startGame();
+    }
+    static void startGame() {
+        String firstPlayerName = "";
+        String secondPlayerName = "";
+        Scanner scanner = new Scanner(System.in);
 
-        int age = 23;
-        String name = "Oleg";
-        double salaryExpectation = 100000;
-        boolean lookingForJob = true;
+        System.out.println("Let's play Tic Tac Toe.");
+        System.out.println("Human vs Human version");
+        System.out.print("First player enter your name: ");
+        String firstUserName = scanner.nextLine();
+        firstPlayerName = firstUserName;
 
-        System.out.println(age);
-        System.out.println(name);
-        System.out.println("salaryExpectation" + salaryExpectation + "$/annually");
-        System.out.println("lookingForJob: " + lookingForJob);
+        System.out.printf("Nice to meet you %s, you are playing with \"X\" \n", firstUserName);
+        System.out.print("Second player enter your name: ");
+        String secondUserName = scanner.nextLine();
+        secondPlayerName = secondUserName;
 
-        Main.loopExample();
-        Main.test();
-        Main.testWithParam(1000);
+        System.out.printf("Nice to meet you %s you are playing with \"O\"\n", secondUserName);
+        System.out.println("\n");
+        System.out.println("(0,0) | (0,1) | (0,2)");
+        System.out.println("------------");
+        System.out.println("(1,0) | (1,1) | (1,2)");
+        System.out.println("------------");
+        System.out.println("(2,0) | (2,1) | (2,2)");
+        System.out.println("\n");
+        System.out.printf("Let's start %s. \nChoose cell x: ", firstPlayerName);
+        int x = scanner.nextInt();
+        System.out.print("Now choose y: ");
+        int y = scanner.nextInt();
+        System.out.printf("%s. Your cell is (%s, %s) \n", firstPlayerName, x, y);
     }
 
-    public static void loopExample() {
-        int[] arrayOfInt = {0, 1, 3, 5, 7};
-        for (int valueOfArray : arrayOfInt) {
-            System.out.println("valueOfArray: " + valueOfArray);
-        }
-    }
-
-    public static void test() {
-        System.out.println("new method call");
-    }
-    public static void testWithParam(int newValue) {
-        System.out.print("print new value form params: ");
-        System.out.print(newValue);
-    }
 }
